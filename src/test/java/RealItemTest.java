6cb00c70a5af19250696d63e8be707599454a3b9
Viktoriia Badenko <viktoriiabadenko@coherentsolutions.com>
2023-08-02 20:00:03 +0300

    Fixed parameters

--- src/test/java/RealItemTest.java
+++ src/test/java/RealItemTest.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import shop.RealItem;
 
@@ -5,11 +6,12 @@ import static org.testng.Assert.assertEquals;
 
 public class RealItemTest {
     @Test(groups = {"cart"})
-    public void testToString() {
+    @Parameters({"name", "price", "weight"})
+    public void testToString(String name, int price, double weight) {
         RealItem realItem = new RealItem();
-        realItem.setName("Laptop");
-        realItem.setPrice(1000);
-        realItem.setWeight(2.5);
+        realItem.setName(name);
+        realItem.setPrice(price);
+        realItem.setWeight(weight);
 
         String actual = realItem.toString();
         String expected = "Class: class shop.RealItem; Name: Laptop; Price: 1000.0; Weight: 2.5";
