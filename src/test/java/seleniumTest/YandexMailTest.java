92daa3593dd17cbf9aabaacd70926bd383a7ef4f
Viktoriia Badenko <viktoriiabadenko@coherentsolutions.com>
2023-08-03 11:56:04 +0300

    Added dependency to pom file

--- src/test/java/seleniumTest/YandexMailTest.java
+++ src/test/java/seleniumTest/YandexMailTest.java
@@ -1,10 +1,26 @@
 package seleniumTest;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class YandexMailTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void testLoginToYandexMail() {
+        driver.get("https://mail.yandex.com/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
     }
 }
